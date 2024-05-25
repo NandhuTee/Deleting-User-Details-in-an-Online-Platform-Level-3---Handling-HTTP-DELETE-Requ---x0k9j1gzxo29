d734d2b445dfd757440e9edebac9bac47ac039ea
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-05-25 23:19:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,33 @@ const userDetails = JSON.parse(
 app.use(express.json());
 
 //Write DELETE endpoint for deleting the details of user
+app.delete("/api/v1/details/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const userToDelete = userDetails.find(
+    (entry) => entry.id === id
+  );
+  if(!userToDelete){
+    
+    res.status(404).json({message: 'User not found!', status: 'failed'});
+    
+    return;
+  }
+
+  const updatedUsers = userDetails.filter((entry) => {
+    entry.id !== id
+    
+  })
+  fs.writeFile(`${__dirname}/data/userDetails.json`, JSON.stringify(updatedUsers), (err) => {
+    
+    res.status(200).json({
+      status: "success",
+      message: "User details deleted successfully",
+      data: {
+        userDetails: userToDelete
+      }
+    })
+  })
+})
 
 // PATCH endpoint for editing user details
 app.patch("/api/v1/details/:id", (req, res) => {
@@ -21,7 +48,9 @@ app.patch("/api/v1/details/:id", (req, res) => {
   const index = userDetails.indexOf(updatedDetails);
   if (!updatedDetails) {
     return res.status(404).send({
+      
       status: "failed",
+      
       message: "User not found!",
     });
   }
